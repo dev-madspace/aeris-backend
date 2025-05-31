4ff3e51ac6a3b0a8cf883f41d0cf1951a07b46d4
dev-madspace <pro.madspace@gmail.com>
2025-06-01 00:54:06 +0530

    Update index.js

--- index.js
+++ index.js
@@ -4,7 +4,7 @@ const http = require('http');
 
 const server = http.createServer((req, res) => {
   res.writeHead(200, { 'Content-Type': 'application/json' });
-  res.end(JSON.stringify({ message: "Hello from your GCP VM backend!" }));
+  res.end(JSON.stringify({ message: "Hey brooo!" }));
 });
 
 const PORT = 3000;
